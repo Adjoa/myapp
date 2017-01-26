748532e837dae46663d7244eb572542491e5f8a9
root <root@appserver.example.local>
2017-01-26 00:39:48 +0000

    added myapp_port and myapp_version params

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,15 @@
-class myapp::config {
+class myapp::config(
+  $myapp_port    = '9090',
+  $myapp_version = 'myapp',
+){
   tomcat::config::server::connector { 'tomcat7-myapp-http':
-    catalina_base         => '/opt/tomcat7/myapp',
-    port                  => '9090',
-    protocol              => 'HTTP/1.1',
+    catalina_base => '/opt/tomcat7/myapp',
+    port          => $myapp_port,
+    protocol      => 'HTTP/1.1',
   }
 
   tomcat::war { 'myapp.war':
     catalina_base => '/opt/tomcat7/myapp',
-    war_source    => "puppet:///modules/myapp/teppup_myapp.war",
+    war_source    => "puppet:///modules/myapp/teppup_${myapp_version}.war",
   }
 }
